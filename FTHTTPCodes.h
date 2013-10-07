bf7f07aab3f8e7b36b0eba02d2eda290169528d0
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-10-07 15:49:13 +0100

    new classes
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ FTHTTPCodes.h
@@ -0,0 +1,19 @@
+//
+//  FTHTTPCodes.h
+//  iJenkins
+//
+//  Created by Ondrej Rafaj on 07/10/2013.
+//  Copyright (c) 2013 Fuerte Innovations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef NS_ENUM(NSInteger, FTHTTPCodesNo) {
+    FTHTTPCodesNo404
+};
+
+
+@interface FTHTTPCodes : NSObject
+
+@end
---
+++ FTHTTPCodes.m
@@ -0,0 +1,14 @@
+//
+//  FTHTTPCodes.m
+//  iJenkins
+//
+//  Created by Ondrej Rafaj on 07/10/2013.
+//  Copyright (c) 2013 Fuerte Innovations. All rights reserved.
+//
+
+#import "FTHTTPCodes.h"
+
+
+@implementation FTHTTPCodes
+
+@end
